7fb2f3c97708e4cfb9b3a5104aea1363f65e33cf
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-17 22:25:57 +0900

    completed

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-const GU    = 1;
-const CHOKI = 2;
-const PA    = 3;
+ function janken (){
+    const GU    = 1;
+    const CHOKI = 2;
+    const PA    = 3;
 
-let hum = prompt('半角数字で1〜3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
-hum = parseInt(hum, 10);
-
-if (hum !== GU && hum !== CHOKI && hum !== PA){
-    alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
-}else{
-    let com = Math.floor(Math.random() * 3) + 1;
-    let comHandName = '';
-    switch (com){
-        case GU:
-            comHandName = 'グー';
-            break;
-        case CHOKI:
-            comHandName = 'チョキ';
-            break;
-        case PA:
-            comHandName = 'パー';
-            break;
+    function getHumHand(){
+        let hum = prompt('半角数字で1〜3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
+        hum = parseInt(hum, 10);
+        if (hum !== GU && hum !== CHOKI && hum !== PA){
+            return undefined;
+        }else{
+            return hum;
+        }
     }
 
-    let msgResult = '';
-    if (hum === com){
-        msgResult = '結果はあいこでした。';
-    }else if ((com === GU & hum === PA) || (com === CHOKI && hum === GU) || (com === PA && hum === CHOKI)){
-        msgResult = '勝ちました。';
-    }else{
-        msgResult = '負けました。';
+    let hum = getHumHand();
+    if (!hum){
+        alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
+    } else {
+        let com = getComHand();
+        alert(getResultMsg(com, hum));
     }
 
-msgResult = msgResult + 'コンピュータの出した手は「' + comHandName + '」でした';
-    alert(msgResult);
+    function getComHand(){
+         return Math.floor(Math.random() * 3) + 1;
+    }
+
+
+    function getHandName(num){
+        switch (num){
+            case GU:
+                return 'グー';
+            case CHOKI:
+                return 'チョキ';
+            case PA:
+                return 'パー';
+        }
+    }
+
+    function getResult (com, hum){
+        if (hum === com){
+            return '結果はあいこでした。';
+        }else if ((com === GU & hum === PA) || (com === CHOKI && hum === GU) || (com === PA && hum === CHOKI)){
+            return '勝ちました。';
+        }else{
+            return '負けました。';
+        }
+    }
+
+    function getResultMsg(com, hum){
+        return getResult(com, hum) + 'コンピュータの出した手は「' + getHandName(com) + '」でした';
+    }   
 }
-
+janken ();
+    
